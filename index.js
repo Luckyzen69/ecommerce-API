babf1370e310609e5058a4c926675e3cf27fa49d
Lagzen Thakuri <thakurizen2@gmail.com>
2023-12-21 12:14:56 +0545

    change in index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const productRoutes = require("./routes/product")
 const handleSeverError = require("./middleware/handleServerError")
 const fileUpload = require("express-fileupload")
 const mongoose = require('mongoose');
-
+      
 mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => console.log('Connected!'));
 
